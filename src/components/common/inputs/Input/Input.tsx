7250ec03509441966eb78655b2447664564e4e66
oresto228 <orest361@gmail.com>
2023-01-04 23:10:18 +0100

    Fix label

--- src/components/common/inputs/Input/Input.tsx
+++ src/components/common/inputs/Input/Input.tsx
@@ -4,7 +4,9 @@ import styles from "./Input.module.scss";
 const Input = () => {
   return (
     <div className={styles.input__block}>
-      <label htmlFor="login">Login</label>
+      <label className={styles.input__label} htmlFor="login">
+        Login
+      </label>
       <input type="text" id="login" className={styles.input} />
       <div className={styles.validation}>Incorrect password</div>
     </div>
