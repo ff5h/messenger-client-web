67de1622d07df8a5fc644aac2d380375fb5c483d
RYBKIN <vdj.manka@gmail.com>
2023-01-04 19:56:53 +0200

    Delete trash files, fix module.scss

---
+++ src/components/common/inputs/Input/Input.tsx
@@ -0,0 +1,11 @@
+import styles from './Input.module.scss';
+
+const Input = () => {
+    return (
+        <input className={styles.wrapper}>
+            Інпут
+        </input>
+    );
+};
+
+export {Input};
