0ca6fe59b00e03a47516da7e6b71fb8b689b708d
oresto228 <orest361@gmail.com>
2023-01-10 19:35:15 +0100

    Styles fix

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { ErrorPage } from "../../pages/ErrorPage";
 import { LoginPage } from "../../pages/LoginPage";
+import { RegistrationPage } from "../../pages/RegistrationPage";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <ErrorPage />
+      <RegistrationPage />
     </div>
   );
 };
